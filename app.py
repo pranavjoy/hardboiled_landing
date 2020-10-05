110293121a01ffae13d59f406631d823ea7d0a7b
Pranav Joy <pranavjoy@gmail.com>
2020-10-05 18:01:20 +0200

    Added uploads

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template("index.html", title="HardBoiled")
+
+@app.route('/uploads/design-brief/')
+def root():
+    return app.send_static_file('uploads/design-brief.pdf')
 
 
 if __name__ == '__main__':
